019b6ca0006b0c9c4c4214d48452b031163c27bb
z <e@mac.mynetworksettings.com>
2024-11-29 17:11:39 -0500

    added some styling and began work on adding goals functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,48 @@
+import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
-
+ 
 function App() {
-  return (
+  const [newGoal, setNewGoal] = useState("");
+  console.log(newGoal)
+  return (  
     <div className="App">
       <header className="App-header">
-        <h1> college fund tracker</h1>
-        <h1> change made in testing-branch</h1>
+        <nav className="navigation"> 
+          {/* <a className="main-heading"> CFT </a> */}
+          <a className="main-heading"> account overview </a>
+          <a className="main-heading"> financial goals </a> 
+          <a className="main-heading"> my account</a>
+        </nav>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="body-container">
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
+        {/* <div className="body-container">
+          <label>add your goal</label> <br></br>
+          <input 
+          value = {newGoal} 
+          onChange = {e => setNewGoal(e.target.value)}
+          type="text" 
+          id="goal"/>
+        </div> */}
       </header>
     </div>
   );
 }
-
 export default App;
+
+
+function Addgoal(){
+  return(
+    <div>
+      here is another task to add
+    </div>
+  );
+}
