7ae46db92b6ec299be2c60ddab5d5a86858af3d3
edwin <e@es-MacBook-Pro.local>
2024-12-04 09:56:49 -0500

    made changes to css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,14 +13,15 @@ import { ConeGeometry } from "three";
 
 // really cool animated emojis on this website
 // https://animated-fluent-emoji.vercel.app/
+
 const TextAnimation = () => {
   return <motion.h2
   initial ={{y : 25 , opacity :0}}
   animate ={{y : 0 , opacity :1}}
-  transition={{duration : 1, ease : "easeInOut"}}
-  
+  transition={{duration : 1, ease : "easeInOut"}}  
   > CollegeTracker <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Eight-Thirty.png" alt="Eight-Thirty" width="60" height="60" /></motion.h2>
 }
+
 function App() {
   return (  
         <Login></Login>
@@ -42,30 +43,33 @@ function LoginInSignUp(){
   )
 }
 
-function Login(){
+function SignUp(){
+  // TODO : component to display when user is signing up for the first time
+  return(
+    null
+  )
+}
 
+function Login(){
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [loginState, setLoginState] = useState(false);
   const [incorrectLogin, setIncorrectLogin] = useState(false);
-  // console.log(incorrectLogin)
+
 
   const validateLogin = (username) => {
-    if(username == 'leticia'){
+    if(username == 'john'){
       setLoginState(true);
     }
-    if(username != 'leticia'){
+    if(username != 'john'){
       setIncorrectLogin(true)
     }
   }
 
   if (loginState)
     return <Success></Success>
-
   return(
-    
     <div className="outer-login-div">
-      
       <img className = "background-image" 
       src='https://wagner.edu/communications/files/2020/03/MainHall4-1920.jpg'>
       </img>
@@ -85,12 +89,11 @@ function Login(){
           type = "text"
           value = {password}
           onChange={(event) => setPassword(event.target.value)}
-          
           />
-        {incorrectLogin && <h3 className='logins'> incorrect login, please try again.</h3>}
-        <button className='login-button' onClick={(event) => validateLogin(username)}> sign in </button>
+          {incorrectLogin ? <h3 className='logins'> incorrect login, please try again.</h3> : null}
+        <button className='login-button' onClick={(event) => validateLogin(username)}> Sign In </button>
       </div>
-      
+      {/* <h1>dont have an account? sign up here</h1> */}
     </div>
   )
 }
