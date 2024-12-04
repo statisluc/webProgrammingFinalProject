e17f1205d2a029bb2e1da1fdb1f5e722abcfc4b1
edwin <e@mac.mynetworksettings.com>
2024-12-04 01:17:18 -0500

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
-import {useState} from "react";
+import React from 'react';
+import {motion} from "framer-motion";
+import { IoMdPerson } from "react-icons/io";import {useState} from "react";
+import { IoIosKey } from "react-icons/io";
 import logo from './logo.svg';
 import './App.css';
 import Home from "./components/Home";
@@ -6,15 +9,97 @@ import Goals from "./components/Goals";
 import Myaccount from "./components/Myaccount";
 import {Route,Routes} from "react-router-dom" ;
 import {Link} from "react-router-dom";
+import { ConeGeometry } from "three";
 
+// really cool animated emojis on this website
+// https://animated-fluent-emoji.vercel.app/
+const TextAnimation = () => {
+  return <motion.h2
+  initial ={{y : 25 , opacity :0}}
+  animate ={{y : 0 , opacity :1}}
+  transition={{duration : 1, ease : "easeInOut"}}
+  
+  > CollegeTracker <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Eight-Thirty.png" alt="Eight-Thirty" width="60" height="60" /></motion.h2>
+}
 function App() {
   return (  
-        <Routes>
-          <Route exact path="/" element={<Home></Home>}></Route>
-          <Route exact path="/goals" element={<Goals></Goals>}></Route>
-          <Route exact path="/myaccount" element={<Myaccount></Myaccount>}></Route>
-        </Routes>
+        <Login></Login>
+      
+  )
+}
+
+function Success(){
+  return(
+    <h1>welcome user</h1>
+  )
+}
+
+function LoginInSignUp(){
+  return(
+    <div className='login-sign-up'>
+    <button className='login-button'>login</button> <button className='login-button'>sign up</button>
+    </div>
+  )
+}
+
+function Login(){
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loginState, setLoginState] = useState(false);
+  const [incorrectLogin, setIncorrectLogin] = useState(false);
+  // console.log(incorrectLogin)
+
+  const validateLogin = (username) => {
+    if(username == 'leticia'){
+      setLoginState(true);
+    }
+    if(username != 'leticia'){
+      setIncorrectLogin(true)
+    }
+  }
+
+  if (loginState)
+    return <Success></Success>
+
+  return(
+    
+    <div className="outer-login-div">
+      
+      <img className = "background-image" 
+      src='https://wagner.edu/communications/files/2020/03/MainHall4-1920.jpg'>
+      </img>
+      <div className="inner-login-div">
+        {/*     <LoginInSignUp></LoginInSignUp> */}
+        <h1 className="login-heading"> <TextAnimation></TextAnimation> </h1>
+        <h2 className='username-password'>Username <IoMdPerson /></h2>
+          <input 
+          className= 'logins'
+          type = "text"
+          value = {username}
+          onChange={(event) => setUsername(event.target.value)}
+          />
+        <h2 className='username-password'> Password <IoIosKey/></h2>
+        <input 
+          className='logins'
+          type = "text"
+          value = {password}
+          onChange={(event) => setPassword(event.target.value)}
+          
+          />
+        {incorrectLogin && <h3 className='logins'> incorrect login, please try again.</h3>}
+        <button className='login-button' onClick={(event) => validateLogin(username)}> sign in </button>
+      </div>
+      
+    </div>
   )
 }
 
 export default App;
+//      depricated for now, goes in App()
+        // <Routes>
+          
+        //   <Route exact path="/" element={<Home></Home>}></Route>
+        //   <Route exact path="/goals" element={<Goals></Goals>}></Route>
+        //   <Route exact path="/myaccount" element={<Myaccount></Myaccount>}></Route>
+        // </Routes>
--- server/index.js
+++ server/index.js
@@ -7,5 +7,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.get('/home', (req, res) => {
+    res.send('Hello home!');
+});
+
+
 app.listen(4000, () => console.log('Server is running on http://localhost:4000'))
 
