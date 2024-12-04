2d47a7b72bb4d938c57169fc461138ac5c57102f
edwin <e@es-MacBook-Pro.local>
2024-12-04 10:47:52 -0500

    conneceted project to database

--- server/index.js
+++ server/index.js
@@ -1,18 +1,43 @@
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg");
+const {Pool} = require('pg');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+    user : 'e',
+    host : 'localhost',
+    database : 'todolist',
+    password : '630487',
+    port : 5432,
+});
 
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+pool
+	.connect()
+	.then(() => {
+		console.log('Connected to PostgreSQL database');
+	})
+	.catch((err) => {
+		console.error('Error connecting to PostgreSQL database', err);
+	});
+
+pool.query('SELECT * FROM todos', (err, result) => {
+    if (err) {
+        console.error('Error executing query', err);
+    } else {
+        console.log('Query result:', result.rows);
+    }
+});    
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 
-app.get('/home', (req, res) => {
+app.get('/home', (req, res) => {    
     res.send('Hello home!');
 });
 
